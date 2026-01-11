11418172f0c4c9320578b782019142dcefcdf871
Atilla <atillactrk@gmail.com>
2026-01-11 22:32:01 +0300

    ..

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,18 +3,15 @@ export const projects = [
     title: "Proje 1",
     description: "Örnek Proje 1",
     tech: ["React", "Vite"],
-    link: "github.com/AtillaCitirik",
   },
   {
     title: "Proje 2",
     description: "Örnek Proje 2",
     tech: ["JavaScript", "CSS"],
-    link: "github.com/AtillaCitirik",
   },
   {
     title: "Proje 3",
     description: "Örnek Proje 3",
     tech: ["API", "UI"],
-    link: "github.com/AtillaCitirik",
   },
 ];
